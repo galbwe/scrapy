2377b8154e2619e345558395d2ea127704fc5ef4
Wes Galbraith <galbwe92@gmail.com>
2025-10-05 11:38:27 -0600

    Refactor httpx tests to subclass from base download handler tests similar to twisted tests

--- tests/test_downloader_handler_httpx.py
+++ tests/test_downloader_handler_httpx.py
@@ -1,55 +1,67 @@
-"""Tests for scrapy.core.downloader.handlers.httpx.HTTPXDownloadHandler."""
+"""Tests for scrapy.core.downloader.handlers.http11.HTTP11DownloadHandler."""
 
 from __future__ import annotations
 
 from typing import TYPE_CHECKING, Any
 
-# Running all pre-existing twisted HTTP download handler tests under HTTPX to avoid writing duplicate tests
-from tests.test_downloader_handler_twisted_http2 import TestHttps2, TestHttp2MockServer
-from tests.test_downloader_handler_twisted_http10 import TestHttp10, TestHttps10
-from tests.test_downloader_handler_twisted_http11 import TestHttp11, TestHttps11
-from tests.test_downloader_handlers_http_base import TestHttpMockServerBase
+from scrapy.core.downloader.handlers.httpx import HTTPXDownloadHandler
+from tests.test_downloader_handlers_http_base import (
+    TestHttp11Base,
+    TestHttpProxyBase,
+    TestHttps11Base,
+    TestHttpsCustomCiphersBase,
+    TestHttpsInvalidDNSIdBase,
+    TestHttpsInvalidDNSPatternBase,
+    TestHttpsWrongHostnameBase,
+    TestHttpWithCrawlerBase,
+    TestSimpleHttpsBase,
+)
 
 if TYPE_CHECKING:
     from scrapy.core.downloader.handlers import DownloadHandlerProtocol
 
-class TestHttps2x(TestHttps2):
+
+class HTTPXDownloadHandlerMixin:
     @property
     def download_handler_cls(self) -> type[DownloadHandlerProtocol]:
-        from scrapy.core.downloader.handlers.httpx import HTTPXDownloadHandler
-
-        return HTTPXDownloadHandler
-    
-
-
-
-class TestHttp11x(TestHttp11):
-    @property
-    def download_handler_cls(self) -> type[DownloadHandlerProtocol]:
-        from scrapy.core.downloader.handlers.httpx import HTTPXDownloadHandler
-
         return HTTPXDownloadHandler
 
 
-class TestHttps11x(TestHttps11):
+class TestHttp11(HTTPXDownloadHandlerMixin, TestHttp11Base):
+    pass
+
+
+class TestHttps11(HTTPXDownloadHandlerMixin, TestHttps11Base):
+    pass
+
+
+class TestSimpleHttps(HTTPXDownloadHandlerMixin, TestSimpleHttpsBase):
+    pass
+
+
+class TestHttps11WrongHostname(HTTPXDownloadHandlerMixin, TestHttpsWrongHostnameBase):
+    pass
+
+
+class TestHttps11InvalidDNSId(HTTPXDownloadHandlerMixin, TestHttpsInvalidDNSIdBase):
+    pass
+
+
+class TestHttps11InvalidDNSPattern(
+    HTTPXDownloadHandlerMixin, TestHttpsInvalidDNSPatternBase
+):
+    pass
+
+
+class TestHttps11CustomCiphers(HTTPXDownloadHandlerMixin, TestHttpsCustomCiphersBase):
+    pass
+
+
+class TestHttp11WithCrawler(TestHttpWithCrawlerBase):
     @property
-    def download_handler_cls(self) -> type[DownloadHandlerProtocol]:
-        from scrapy.core.downloader.handlers.httpx import HTTPXDownloadHandler
-
-        return HTTPXDownloadHandler
+    def settings_dict(self) -> dict[str, Any] | None:
+        return None  # default handler settings
 
 
-class TestHttp10x(TestHttp10):
-    @property
-    def download_handler_cls(self) -> type[DownloadHandlerProtocol]:
-        from scrapy.core.downloader.handlers.httpx import HTTPXDownloadHandler
-
-        return HTTPXDownloadHandler
-
-
-class TestHttps10x(TestHttps10):
-    @property
-    def download_handler_cls(self) -> type[DownloadHandlerProtocol]:
-        from scrapy.core.downloader.handlers.httpx import HTTPXDownloadHandler
-
-        return HTTPXDownloadHandler
+class TestHttp11Proxy(HTTPXDownloadHandlerMixin, TestHttpProxyBase):
+    pass
